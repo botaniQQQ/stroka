b39ff8ed8a67caa575f5e3bfa55edb2e1474c9a0
botaniQQQ <botaniq3@gmail.com>
2019-12-15 07:03:03 +0200

    BB-codes, mapping tags

--- README.md
+++ README.md
@@ -2,14 +2,57 @@
 
 ```js
 const stroka = require('stroka');
-console.log(stroka('Chip&amp;Dale'));
-//=> Chip&Dale
-```
 
-### Clear HTML
+let text = '[b] Chip [/b] &amp; <span> Dale </span>';
 
-```js
-const stroka = require('stroka');
-console.log(stroka('Chip&amp;<span>Dale</span>', true));
-//=> Chip&Dale
+console.log(stroka(text));
+//=> Chip & Dale
+
+console.log(stroka(text, {decode_text: false}));
+//=> Chip &amp; Dale
+
+console.log(stroka(text, {clear_html_tags: false}));
+//=> Chip & <span> Dale </span>
+
+console.log(stroka(text, {clear_bb_codes: false}));
+//=> [b] Chip [/b] & Dale
+
+console.log(stroka(text, {create_map_tags: true}));
+//=> {
+//=>   text: '_4_',
+//=>   tags: [ '[b]', '[/b]', '<span>', '</span>', '_0_ Chip _1_ & _2_ Dale _3_' ],
+//=>   options: { create_map_tags: true }
+//=> }
+
+console.log(stroka(text, {create_map_tags: true, min_chars_between_tags: 4}));
+//=> {
+//=>   text: '_0_ Chip _4_ Dale _3_',
+//=>   tags: [ '[b]', '[/b]', '<span>', '</span>', '_1_ & _2_' ],
+//=>   options: { create_map_tags: true, min_chars_between_tags: 4 }
+//=> }
+
+console.log(stroka(text, {create_map_tags: true, min_chars_between_tags: 4, merging_tags: false}));
+//=> {
+//=>   text: '_0_ Chip _1_ & _2_ Dale _3_',
+//=>   tags: [ '[b]', '[/b]', '<span>', '</span>' ],
+//=>   options: { create_map_tags: true, min_chars_between_tags: 4, merging_tags: false }
+//=> }
+
+console.log(stroka(text, {create_map_tags: true, min_chars_between_tags: 4, symbol_side_left: "$", symbol_side_right: ""}));
+//=> {
+//=>   text: '$0 Chip $4 Dale $3',
+//=>   tags: [ '[b]', '[/b]', '<span>', '</span>', '$1 & $2' ],
+//=>   options: {
+//=>     create_map_tags: true,
+//=>     min_chars_between_tags: 4,
+//=>     symbol_side_left: '$',
+//=>     symbol_side_right: ''
+//=>   }
+//=> }
+
+console.log(stroka(stroka(text, {create_map_tags: true, min_chars_between_tags: 4})));
+//=> [b] Chip [/b] & <span> Dale </span>
+
+console.log(stroka(stroka(stroka(text, {create_map_tags: true, min_chars_between_tags: 4}))));
+//=> Chip & Dale
 ```
--- index.js
+++ index.js
@@ -1,22 +1,102 @@
 var entities = new (require('html-entities').AllHtmlEntities)();
 
-module.exports = function (text, html) {
-    var out = '', arr, i = 0, l, x;
-    if (!text) return out;
-    text = entities.decode(text);
-    arr = text.split(/(%(?:D0|D1)%.{2})/);
-    for (l = arr.length; i < l; i++) {
-        try {
-            x = decodeURIComponent(arr[i]);
-        } catch (e) {
-            x = arr[i];
+module.exports = function (text, options) {
+    if (!text) return '';
+    var result = '', tags = [];
+    options = options || {};
+    if (typeof text === 'object') {
+        if (
+            typeof text.text === 'string' &&
+            typeof text.tags === 'object' &&
+            text.text && text.tags.length
+        ) {
+            options = typeof text.options === 'object'
+                ? text.options
+                : options;
+            tags = text.tags;
+            text = text.text;
+            options.create_map_tags = true;
+        } else {
+            text = typeof text.text === 'string' && text.text
+                ? text.text
+                : '';
         }
-        out += x;
     }
-    if (html) {
-        out = out.replace(/(<.*?>)/g, '');
+    result = text || '';
+    var opts = Object.assign({
+        decode_text: true,
+        clear_html_tags: true,
+        clear_bb_codes: true,
+        create_map_tags: false,
+        merging_tags: true,
+        min_chars_between_tags: 30,
+        symbol_side_left: '_',
+        symbol_side_right: '_'
+    }, typeof options === 'object' ? options : {});
+    if (opts.decode_text) {
+        result = decodeText(result);
     }
-    return out
+    if (opts.clear_html_tags && !opts.create_map_tags) {
+        result = result.replace(/(<[^<\r\n]*?>)/g, '');
+    }
+    if (opts.clear_bb_codes && !opts.create_map_tags) {
+        result = result.replace(/(\[[^\[\r\n]*?])/g, '');
+    }
+    if (opts.create_map_tags) {
+        var c = opts.min_chars_between_tags;
+        var l = opts.symbol_side_left
+            ? opts.symbol_side_left.replace(/([$^.\[()?*\\|])/g, '\\$1')
+            : '';
+        var r = opts.symbol_side_right
+            ? opts.symbol_side_right.replace(/([$^.\[()?*\\|])/g, '\\$1')
+            : '';
+        var n = l !== r ? l + r : l;
+        if (tags.length) {
+            var all = tags.length - 1;
+            tags.slice().reverse().forEach(function (tag, i) {
+                var rgx = new RegExp((l ? l + '\\s?' : '') + (all - i) + (r ? '\\s?' + r : ''), 'ig');
+                result = result.replace(rgx, tag);
+            });
+        } else {
+            var rgx1 = /(<[^<\r\n]*?>|\[[^\[\r\n]*?])/g;
+            if (result.indexOf('<') === -1 && result.indexOf('[') === -1) {
+                return {
+                    text: clearSpace(result),
+                    tags: tags,
+                    options: options
+                };
+            } else if (result.indexOf('<') !== -1 && result.indexOf('[') === -1) {
+                rgx1 = /(<[^<\r\n]*?>)/g;
+            } else if (result.indexOf('<') === -1 && result.indexOf('[') !== -1) {
+                rgx1 = /(\[[^\[\r\n]*?])/g;
+            }
+            result = clearSpace(result).replace(rgx1, function(match, tag) {
+                var io1 = tags.indexOf(tag);
+                if (io1 !== -1) return ' ' + opts.symbol_side_left + io1 + opts.symbol_side_right + ' ';
+                tags.push(tag);
+                return ' ' + opts.symbol_side_left + (tags.length - 1) + opts.symbol_side_right + ' ';
+            });
+            if (opts.merging_tags) {
+                var rgx2 = new RegExp('(^[^' + n + ']{1,' + c + '}' + l + '[0-9]+?' + r + '([^' + n + ']{1,' + c + '}' + l + '[0-9]+?' + r + ')*|(' + l + '[0-9]+?' + r + '[^' + n + ']{1,' + c + '})*' + l + '[0-9]+?' + r + '[^' + n + ']{1,' + c + '}$|' + l + '[0-9]+?' + r + '([^' + n + ']{1,' + c + '}' + l + '[0-9]+?' + r + ')+)', 'g');
+                result = clearSpace(result).replace(rgx2, function(match, tag) {
+                    var io2 = tags.indexOf(tag);
+                    if (io2 !== -1) return ' ' + opts.symbol_side_left + io2 + opts.symbol_side_right + ' ';
+                    tags.push(tag);
+                    return ' ' + opts.symbol_side_left + (tags.length - 1) + opts.symbol_side_right + ' ';
+                });
+            }
+            return {
+                text: clearSpace(result),
+                tags: tags,
+                options: options
+            };
+        }
+    }
+    return clearSpace(result);
+};
+
+function clearSpace(text) {
+    return text
         .replace(/[ \f\t\v​\u00a0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​​\u202f\u205f​\u3000]+/g, ' ')
         .replace(/(\r?\n)+/g, '\n')
         .replace(/\n+/g, '\n')
@@ -25,5 +105,20 @@ module.exports = function (text, html) {
         .replace(/\r+/g, '\r')
         .replace(/\r /g, '\r')
         .replace(/ \r/g, '\r')
-        .replace(/(^\s*)|(\s*)$/g, '')
-};
+        .replace(/(^\s*)|(\s*)$/g, '');
+}
+function decodeText(text) {
+    var result = '', arr = [], i = 0, l, x;
+    if (!text) return result;
+    text = entities.decode(text);
+    arr = text.split(/(%(?:D0|D1)%.{2})/);
+    for (l = arr.length; i < l; i++) {
+        try {
+            x = decodeURIComponent(arr[i]);
+        } catch (e) {
+            x = arr[i];
+        }
+        result += x;
+    }
+    return result;
+}
